625e0955ca175e9c0a35d2b5ca8aa674bb685f5f
Aniket Choudhary <56171163+aniketsonu@users.noreply.github.com>
2023-08-11 19:06:37 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { connect } from "react-redux"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Author {
+  name: string
+  books: string[]
 }
 
-export default App;
+interface TurnData {
+  author: Author
+  books: string[]
+}
+
+function Hero() {
+  return (
+    <div className="flex flex-col items-center p-4 m-2 rounded-md bg-slate-100 w-[50%]">
+      <div>
+        <h1 className="text-gray-900 font-bold text-2xl font-serif">
+          Author Quiz
+        </h1>
+      </div>
+      <div>
+        <p>Select the book written by author</p>
+      </div>
+    </div>
+  )
+}
+
+function Turn({
+  author,
+  books,
+}: {
+  author: Author
+  books: string[]
+  change: (color: string) => void
+}) {
+  const [btnColor, setBtnColor] = React.useState("")
+  const [hoverColor, setHoverColor] = React.useState("hover:bg-gray-300")
+  const [clickedButtons, setClickedButtons] = React.useState("")
+
+  // Function to handle button click
+  const handleButtonClick = (title: string, books: string[]) => {
+    // Check if the button is already clicked
+    setBtnColor(books.includes(title) ? "bg-green-600 text-white" : "bg-red-600 text-white");
+    setHoverColor(books.includes(title) ? "hover:bg-green-700" : "hover:bg-red-700");
+    setClickedButtons(title)
+  }
+
+  return (
+    <div className="flex flex-col items-center m-4 gap-2">
+      <div>
+        <p>{author.name}</p>
+      </div>
+      <div>
+        {books.map((title) => (
+          <p
+            className={`${
+              clickedButtons.includes(title) ? btnColor : "bg-white-200"
+            } ${clickedButtons.includes(title) ? hoverColor: "hover:bg-gray-200"} border-2 border-blue-400 mt-2 p-4 w-80 rounded-md hover:cursor-pointer `}
+            key={title}
+            onClick={() => {
+              handleButtonClick(title, author.books)
+            }}
+          >
+            {title}
+          </p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Continue() {
+  return <></>
+}
+
+const mapStateToProps = (state: { turnData: TurnData }) => {
+  return {
+    turnData: state.turnData,
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    onAnswerSelected: () => {},
+  }
+}
+
+const AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)
+function App({ turnData }: { turnData: TurnData }) {
+  const [bgColor, setBgColor] = React.useState("bg-white-500")
+
+  return (
+    <div className={`${bgColor} flex flex-col items-center`}>
+      <Hero />
+      <Turn
+        {...turnData}
+        change={(color: string) => {
+          setBgColor(color)
+        }}
+      />
+      <Continue />
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,83 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as ReactRedux from 'react-redux';
+import * as Redux from 'redux';
+
+const reducer = (state = {}, action: any) => {
+  return state
+}
+
+let store = Redux.createStore(reducer)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const authors = [
+  {
+    name: 'Mark Twain',
+    books: [
+      'The Adventures of Huckleberry Finn',
+    ]
+  },
+  {
+    name: 'Joseph Conrad',
+    books: [
+      'Heart of Darkness',
+    ]
+  },
+  {
+    name: 'J.K. Rowling',
+    books: [
+      'Harry Potter and the Sorcerers Stone'
+    ]
+  },
+  {
+    name: 'Stephen King',
+    books: [
+      'The Shining',
+      'IT'
+    ]
+  },
+  {
+    name: 'Charles Dickens',
+    books: [
+      'David Copperfield',
+      'A Tale of Two Cities'
+    ]
+  },
+  {
+    name: 'William Shakespeare',
+    books: [
+      'Hamlet',
+      'Macbeth',
+      'Romeo and Juliet'
+    ]
+  }
+]
+
+
+function getTurnData(authors: { name: string, books: string[] }[]) {
+  const allBooks = authors.reduce((p, c) => [...p, ...c.books], [] as string[])
+  const fourRandomBooks = allBooks.sort(() => 0.5 - Math.random()).slice(0, 4)
+  const answer = fourRandomBooks[Math.floor(Math.random() * 3)]
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) => author.books.some((title) => title === answer))!
+  }
+}
+
+
+const state = {
+  turnData: getTurnData(authors),
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ReactRedux.Provider store={store}>
+      <App {...state}/>
+    </ReactRedux.Provider>
   </React.StrictMode>
 );
 
